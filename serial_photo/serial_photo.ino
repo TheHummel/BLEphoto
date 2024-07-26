12269e4bd2fc39a631fc2a4f10543a6a08df1a6b
jannisalsbach <jannisalsbach@gmail.com>
2024-07-26 18:09:27 +0200

    set higher resolution

--- serial_photo/serial_photo.ino
+++ serial_photo/serial_photo.ino
@@ -31,7 +31,7 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_QVGA;
+  config.frame_size = FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
